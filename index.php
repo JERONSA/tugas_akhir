97a973d74fc4cc482add18b8d974dd92d466e0d9
unknown <jeniroan11061980@gmail.com>
2020-06-14 10:01:05 +0800

    membuat halaman login

---
+++ index.php
@@ -0,0 +1,25 @@
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Halaman Admin</title>
+ </head>
+ <body>
+ <h1>Daftar Sepatu</h1>	
+ <table border="1" cellpadding="10" cellspacing="0">
+ <tr>
+ 	<th>No.</th>
+ 	<th>Aksi</th>
+ 	<th>Gambar</th>
+ 	<th>Merk</th>
+ 	<th>Harga</th>
+ 	<th>Pengiriman</th>
+ 	<th>Variation</th>
+ 	<th>Pembayaran</th>
+ </tr>
+ 	</table>
+  </body>
+ </html>
+
+
+ 
