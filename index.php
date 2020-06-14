7f5cb3753f82cda4b4cead0dc85c602187b98257
unknown <jeniroan11061980@gmail.com>
2020-06-14 10:15:02 +0800

    menampilkan data lengkap

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
  <td>
  	<img src="img/sepatu1.jpg" width="50">
  </td>
+ <td>Jimmy Choo</td>
+ <td>695.000</td>
+ <td>Luar Negeri</td>
+ <td>36,37,38,39,40</td>
+ <td>Bisa COD</td>
  </tr>
  	</table>
   </body>
