22d12b5fea88c687c03d3908488fdf95c03d5be3
unknown <jeniroan11061980@gmail.com>
2020-06-14 11:09:13 +0800

    menggunakan fetch array

--- index.php
+++ index.php
@@ -21,8 +21,9 @@ $result = mysqli_query($koneksi,"SELECT * FROM data_spt");
 //mysqli_fetch_array();
 //mysqli_fetch_object();
 
-$spt = mysqli_fetch_assoc($result);
-var_dump($spt['harga']);
+$spt = mysqli_fetch_array($result);
+var_dump($spt);
+
 
 
 
